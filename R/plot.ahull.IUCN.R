84dd79ef8dccb4ab0ce5e5b190391219a3a818af
Beatriz Pateiro-López <beatriz.pateiro@usc.es>
2023-04-14 16:24:43 +0200

    Allows to remove segments and isolated points

--- R/plot.ahull.IUCN.R
+++ R/plot.ahull.IUCN.R
@@ -1,5 +1,6 @@
 #' Very simple plot function
 #' @param x The ahull.IUCN object.
+#' @param wplot "Which in the plot?". I.e. should all points segments and connected components be plat (wlines='all', the default), should only connected components be plotted (wlines='comp')
 #' @param ... Additional plot parameters.
 #' @return None.
 #' @export
@@ -15,19 +16,23 @@
 #' plot(ah)
 
 
-plot.ahull.IUCN<-function(x,...){
+plot.ahull.IUCN<-function(x, wplot = c("all", "comp"), ...){
   ah.IUCN.obj<-x
   x <- ah.IUCN.obj$x
   tri.ah<-ah.IUCN.obj$tri.ah.IUCN
   edges.ah<-ah.IUCN.obj$bd.ah.IUCN
   ip.ah<-ah.IUCN.obj$ip.ah.IUCN
 
-  plot(x,main=paste("IUCN ahull for alpha =",ah.IUCN.obj$alpha,"\n Area =",round(ah.IUCN.obj$area,digits=4)),xlab="",ylab="",...)
+  plot(x,main=paste("IUCN ahull for alpha =",ah.IUCN.obj$alpha,"\n Area =",round(ah.IUCN.obj$area,digits=4)),xlab="",ylab="",type="n",...)
   if(nrow(tri.ah)>0){
     for(i in 1:nrow(tri.ah)){
       graphics::polygon(x[tri.ah[i,],1],x[tri.ah[i,],2],col=3,lty=2)       # Triangles in the ahull
     }
   }
+  wplot <- match.arg(wplot)
+  plot.all <- switch(wplot, all = TRUE, comp=FALSE)
+  if(plot.all){
   graphics::segments(x[edges.ah[,1],1],x[edges.ah[,1],2],x[edges.ah[,2],1],x[edges.ah[,2],2],col=4,lwd=3) # Boundary edges in the ahull in blue
   graphics::points(x[ip.ah,],col=2,pch=19)  # Isolated points in the ahull in red (if any)
+  }
 }
